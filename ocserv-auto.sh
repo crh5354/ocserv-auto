6b7c16190c55e7e52bc103cbbd3ccd60099a5d6f
ConquerWorld <crh5354@gmail.com>
2019-03-18 20:26:19 +0800

    Update ocserv-auto.sh
    update

--- ocserv-auto.sh
+++ ocserv-auto.sh
@@ -227,21 +227,6 @@ function ConfigFirewall {
     fi
 }
 
-function Install-http-parser {
-    if [[ $(rpm -q http-parser | grep -c "http-parser-2.0") = 0 ]]; then
-        mkdir -p /tmp/http-parser-2.0 /opt/lib
-        cd /tmp/http-parser-2.0
-        wget "http://mirrors.aliyun.com/epel/7/x86_64/h/http-parser-2.0-5.20121128gitcd01361.el7.x86_64.rpm"
-        rpm2cpio http-parser-2.0-5.20121128gitcd01361.el7.x86_64.rpm | cpio -div
-        mv usr/lib64/libhttp_parser.so.2* /opt/lib
-        sed -i 'N;/Type=forking/a\Environment=LD_LIBRARY_PATH=/opt/lib' /lib/systemd/system/ocserv.service
-        sed -i 'N;/Type=forking/a\ExecStartPost=/bin/sleep 0.1' /lib/systemd/system/ocserv.service
-        systemctl daemon-reload
-        cd ~
-        rm -rf /tmp/http-parser-2.0
-    fi
-}
-
 function ConfigSystem {
     #关闭selinux
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
@@ -302,7 +287,6 @@ PrintEnvironmentVariable
 InstallOcserv
 ConfigOcserv
 ConfigFirewall
-#Install-http-parser
 ConfigSystem
 PrintResult
 
